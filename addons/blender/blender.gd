addfadd8481e6a0c745e52ed8c7163391269103f
LunarEclipse <949886@qq.com>
2025-08-09 05:11:08 +0800

    Update blender.gd

--- addons/blender/blender.gd
+++ addons/blender/blender.gd
@@ -1,14 +1,16 @@
 @tool
 extends EditorPlugin
 
+const BLENDER_LINUX_PATH: String = "/usr/bin/blender"
+
 func _enter_tree() -> void: 
 	# Only run on Linux
 	if OS.get_name() != "Linux": return
 
 	# Initialization of the plugin goes here.
 	var settings: EditorSettings = EditorInterface.get_editor_settings()
-	var blender_path: String     = "/usr/bin/blender"
-	settings.set_setting("filesystem/import/blender/blender_path", blender_path)
+	settings.set_setting("filesystem/import/blender/blender_path", BLENDER_LINUX_PATH)
+	print("Blender path set to: ", BLENDER_LINUX_PATH)
 	pass
 
 func _exit_tree() -> void:
